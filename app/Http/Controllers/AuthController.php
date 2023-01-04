3d0f588731acbe730240eb7159a47ea88eb91857
JCampos44 <j_campos.v@outlook.com>
2023-01-03 23:42:45 -0300

    mejor mensaje de error 401 para login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,7 +30,7 @@ class AuthController extends Controller
     {
         if (!Auth::attempt($request->only('email', 'password'))) {
             return response([
-                'message' => 'No autorizado'
+                'message' => 'Correo o contraseña incorrectos'
             ], 401);
         }
 
